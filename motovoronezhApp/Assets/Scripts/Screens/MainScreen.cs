500c871708d0bde4a484c3d1a14cff2511dce371
e.kravchenko <e.kravchenko@corp.mail.ru>
2022-02-06 00:23:37 +0300

    нижняя панель главного окна(верстка)

---
+++ motovoronezhApp/Assets/Scripts/Screens/MainScreen.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace Windows
+{
+	public class MainScreen : Screen
+	{
+		[Serializable]
+		public class MainScreenTabData
+		{
+			public GameObject Button;
+			public GameObject Panel;
+			public GameObject ButtonImage;
+		}
+
+		[Header("Tabs")]
+		[SerializeField]
+		private MainScreenTabData[] _tabDatas;
+
+		private GameObject _activePanel;
+
+		private void OnEnable()
+		{
+			_activePanel = _tabDatas[0].Panel;
+			for (int i = 1; i < _tabDatas.Length; i++)
+			{
+				_tabDatas[i].Panel.SetActive(false);
+			}
+		}
+
+		public void OnBottomPanelButtonClick(GameObject button)
+		{
+			var data = Array.Find(_tabDatas, tabData => tabData.Button == button);
+			if (data != null)
+			{
+				_activePanel.SetActive(false);
+				_activePanel = data.Panel;
+				_activePanel.SetActive(true);
+			}
+		}
+		
+	}
+}
