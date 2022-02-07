5a0de46269c362fb39912098cebcd467d4ac0b6a
e.kravchenko <e.kravchenko@corp.mail.ru>
2022-02-07 20:58:33 +0300

    итема для вкладки новостей

---
+++ motovoronezhApp/Assets/Scripts/Screens/Items/NewItem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Screens.Items
+{
+	public class NewItem : MonoBehaviour
+	{
+
+		[SerializeField] 
+		private Text _title;
+		
+		[SerializeField] 
+		private Text _date;
+		
+		[SerializeField] 
+		private Text _text;
+
+		public void SetData(string title, string date, string text)
+		{
+			_title.text = title;
+			_date.text = date;
+			_text.text = text;
+		}
+	}
+}
---
+++ motovoronezhApp/Assets/Scripts/Screens/MainScreenPanels/NewsPanel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Screens.Items;
+using UnityEngine;
+
+namespace Screens.MainScreenPanels
+{
+	public class NewsPanel : MonoBehaviour
+	{
+		[SerializeField]
+		private GameObject _content;
+
+		[SerializeField] 
+		private GameObject _newPrefab;
+
+		[SerializeField] 
+		private GameObject _separator;
+		
+		private void OnEnable()
+		{
+			var sep =  Instantiate(_separator, _content.transform);
+			var item = Instantiate(_newPrefab, _content.transform);
+			item.GetComponent<NewItem>().SetData("asasd", "17.02.2020", "sdgadgfsoaisdgfjoafigjoiasjgoiasjdgfoiajdfsg");
+			var sep1 =  Instantiate(_separator, _content.transform);
+			var item1 = Instantiate(_newPrefab, _content.transform);
+			item1.GetComponent<NewItem>().SetData("dfasdfas", "17.03.2020", "aaaaaassdfasdgadgfsoaisdgfjoafigjoiasjgoiasjdgfoiajdfsg");
+			var sep3 =  Instantiate(_separator, _content.transform);
+			for (int i = 0; i < 20; i++)
+			{
+				var item2 = Instantiate(_newPrefab, _content.transform);
+				item2.GetComponent<NewItem>().SetData("dfasdfas", "17.03.2020", "aaaaaassdfasdgadgfsoaisdgfjoafigjoiasjgoiasjdgfoiajdfsg");
+				var sep4 =  Instantiate(_separator, _content.transform);
+			}
+		}
+	}
+}
