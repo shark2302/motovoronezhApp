3470dfd426ebe96120eaf7bba919361ba724718d
Евгений Кравченко <e.kravchenko@corp.mail.ru>
2022-05-06 23:17:54 +0300

    Исправление бага с отображением картинки большого разрешения + отступы в новостях

--- motovoronezhApp/Assets/Scripts/Screens/Items/ImageItem.cs
+++ motovoronezhApp/Assets/Scripts/Screens/Items/ImageItem.cs
@@ -24,9 +24,10 @@ namespace Screens.Items
                 {
                     if (texture != null)
                     {
-                        _layoutElement.minWidth= texture.width;
+                        var width = texture.width > 1250 ? 100 : texture.width; 
+                        _layoutElement.minWidth= width;
                         _layoutElement.minHeight = texture.height;
-                        _rectTransform.sizeDelta = new Vector2(texture.width, texture.height);
+                        _rectTransform.sizeDelta = new Vector2(width, texture.height);
                         _image.sprite = Sprite.Create((Texture2D) texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100.0f);
                     }
                     else
